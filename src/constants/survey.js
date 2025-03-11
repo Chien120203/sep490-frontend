b924e4e3dbaeeae8ee974a9328b5515dbc450856
khanhnguyenduy2105 <khanhndhe173559@fpt.edu.vn>
2025-03-11 21:53:04 +0700

    [khanhnd]refactor: update details form

---
+++ src/constants/survey.js
@@ -0,0 +1,19 @@
+export const RECEIVE_STATUS = 0;
+
+export const STATUSES = [
+    'Receive Request',
+    'Planning',
+    'InProgress',
+    'Completed',
+    'Paused',
+    'Closed'
+];
+
+export const STATUS_LABELS = [
+    'project.statuses.receive_reqs',
+    'project.statuses.planning',
+    'project.statuses.in_progress',
+    'project.statuses.complete',
+    'project.statuses.paused',
+    'project.statuses.close',
+]
